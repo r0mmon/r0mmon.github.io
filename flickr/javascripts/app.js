b6b39dd38fa4f2ea73bbc8cafce8eb9ef865172e
r0mmon <rommonmail@gmail.com>
2017-01-16 18:23:00 +0300

    fix buttons text

--- flickr/javascripts/app.js
+++ flickr/javascripts/app.js
@@ -1,6 +1,6 @@
 var main = function() {
     "use strict";
-    // console.log("Hello");
+    var messages = ["первое сообщение", "второе сообщение", "третье", "четвертое"];
     var tag = ""; //Запрос по тэгу
     var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + tag + "&format=json&jsoncallback=?";
     $.getJSON(url, function(flickrResponse) {
@@ -13,5 +13,22 @@ var main = function() {
             $img.fadeIn(3000);
         });
     });
+    var displayMessage = function (messageIndex) {
+    // создаем и скрываем элемент DOM
+           var $message = $("<p>").text(messages[messageIndex]).hide();
+           // очищаем текущее содержимое
+           // лучше всего будет выделить текущий параграф
+           // и постепенно скрыть его
+           $(".message").empty();
+           // добавляем сообщение с messageIndex вDOM
+           $(".message").append($message);
+           // постепенное отображение сообщения
+           $message.fadeIn();
+           setTimeout(function () {
+            // через 3 секунды вызываем displayMessage снова со следующим индексом
+            messageIndex = messageIndex + 1;
+            displayMessage(messageIndex);
+            }, 3000); };
+       displayMessage(0);
 };
 $(document).ready(main);
