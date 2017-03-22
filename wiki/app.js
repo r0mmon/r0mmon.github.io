b5f2dccc73b643fb2ec70f59f169b2967e7c3147
r0mmon <rommonmail@gmail.com>
2017-03-22 19:36:58 +0300

    Fix broken links

--- wiki/app.js
+++ wiki/app.js
@@ -4,10 +4,16 @@ var wiki = new Vue({
   el: '#root',
   data: {
     message: '',
-    res: []
+    res: [],
+    styleSearch: {
+      marginTop: '250px'
+    }
   },
   methods: {
     getResult: function () {
+      if (this.message === '') {
+        return
+      }
       $.ajax( {
         url: 'http://ru.wikipedia.org/w/api.php',
         data:  {
@@ -20,14 +26,22 @@ var wiki = new Vue({
         success: function(responce) {
           wiki.res = [];
           wiki.message = '';
-          for (var i = 0; i < responce[2].length; i++) {
-            var x = {};
-            x.title = responce[1][i];
-            x.content = responce[2][i];
-            x.url = responce[3][i];
-            wiki.res.push(x);
+          wiki.styleSearch.marginTop = '0px';
+
+          function doSetTimeout(i) {
+            setTimeout(function() {
+              var x = {};
+              x.title = responce[1][i];
+              x.content = responce[2][i];
+              x.url = responce[3][i];
+              wiki.res.push(x);
+            }, 150*i);
           }
-        }
+
+          for (var i = 0; i < responce[2].length; i++) {
+            doSetTimeout(i);
+            }
+          }
       });
     }
   }
